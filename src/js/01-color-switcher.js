8532d74da4dd5d2b1dfb4591efeac39f7418cae8
NataliiaGalanjuk <Tasha.Galanjuk@gmail.com>
2022-04-09 21:57:52 +0200

    Summary Homework 9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+const bodyColor = document.querySelector('body');
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+    startBtn.setAttribute('disabled', true);
+    stopBtn.removeAttribute('disabled');
+});
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+;
+    startBtn.removeAttribute('disabled');
+    stopBtn.setAttribute('disabled', true);
+});
 
